14c0e3f7f4ef2acc25bac753023d2ee9bc409259
Ailton Moreira <amv265@hotmail.com>
2022-04-02 18:03:23 +0100

    initial deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import perfil from './assets/perfil.jpg'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div
+            className="w-full h-screen bg-gradient-to-r from-cyan-500 to-blue-500 bg-local flex flex-col mx-auto items-center justify-center align-middle">
+
+
+
+            <p className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 cursor-pointer mx-4 p-2 rounded-lg">
+                Welcome to Ailton Moreira web page
+            </p>
+
+            <div className="mx-auto w-48 h-auto my-10">
+                <img src={perfil} alt={"Logo"} />
+            </div>
+
+            <div className="my-2 animate-bounce hover:animate-ping text-indigo-500 hover:cursor-pointer capitalize hover:uppercase">
+                Coming very soon....
+
+            </div>
+
+
+        </div>
+    );
 }
 
 export default App;
