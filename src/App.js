d8ebe2fb713db94fb5b97870296f765308dcddd6
Ailton Moreira <amv265@hotmail.com>
2022-04-02 18:14:32 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import perfil from './assets/perfil.jpg'
 
 function App() {
     return (
-        <div
-            className="w-full h-screen bg-gradient-to-r from-cyan-500 to-blue-500 bg-local flex flex-col mx-auto items-center justify-center align-middle">
-
-
+        <div className="w-full h-screen bg-gradient-to-r from-cyan-500 to-blue-500 bg-local flex flex-col mx-auto items-center justify-center align-middle">
 
             <p className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 cursor-pointer mx-4 p-2 rounded-lg">
                 Welcome to Ailton Moreira web page
